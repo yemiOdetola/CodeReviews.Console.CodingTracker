94c95d7045e01b631af1285953c5abb4f13053d4
Odetola Azeez Opeyemi <yemiotola@gmail.com>
2024-11-25 11:49:38 +0100

    Cleared record enum 'req' warning

--- CodingTracker.yemiOdetola/CodingSession.cs
+++ CodingTracker.yemiOdetola/CodingSession.cs
@@ -3,7 +3,7 @@ namespace CodingTracker.yemiOdetola;
 public class CodingSession
 {
   public int Id { get; set; }
-  public string StartTime { get; set; }
-  public string EndTime { get; set; }
+  public required string StartTime { get; set; }
+  public required string EndTime { get; set; }
   public int Duration { get; set; }
 }
